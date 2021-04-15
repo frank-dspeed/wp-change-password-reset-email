2015286c5571d7a5d3a8113b14de0536c61e8f41
Frank Lemanschik <frank@lemanschik.com>
2021-04-15 12:57:13 +0200

    Update functions-snippets.php
    Fixed typos

--- functions-snippets.php
+++ functions-snippets.php
@@ -10,7 +10,7 @@ Sollten Sie die Änderung nicht veranlasst haben, so ignorieren Sie einfach dies
 
 Viele Grüße
 
-Ihr Webmaster'
+Ihr Webmaster';
 
 // Change the title of the password reset e-mail
 function nxt_password_reset_title($title) {
@@ -21,6 +21,6 @@ add_filter('retrieve_password_title', 'nxt_password_reset_title');
 // Change the body text of the password reset e-mail
 function nxt_retrieve_password_message( $message, $key, $user_login) {
 	$password_reset_link = network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' );
-	return str_replace($message_template, "${password_reset_link}", $password_reset_link);;
+	return str_replace($message_template, "${password_reset_link}", $password_reset_link);
 }
 add_filter( 'retrieve_password_message', 'nxt_retrieve_password_message', 10, 4 );
