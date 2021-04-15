fb09f4d8bc6c7efabd5e1bf969e241079844a27f
Frank Lemanschik <frank@lemanschik.com>
2021-04-15 13:09:43 +0200

    Update functions-snippets.php

--- functions-snippets.php
+++ functions-snippets.php
@@ -1,6 +1,16 @@
 <?php
-$message_title = 'Passwort zurücksetzen für die Webseite abc.com';
-$message_template = 'Hallo,
+
+// Change the title of the password reset e-mail
+function nxt_password_reset_title($title) {
+	$message_title = 'Passwort zurücksetzen für die Webseite abc.com';
+	return $message_title;
+}
+add_filter('retrieve_password_title', 'nxt_password_reset_title');
+
+// Change the body text of the password reset e-mail
+function nxt_retrieve_password_message( $message, $key, $user_login) {
+
+	$message_template = 'Hallo,
 
 jemand hat die Änderung Ihres Passworts beantragt. Um diese Änderung zu bestätigen, klicken Sie bitte auf den folgenden Link:
 
@@ -11,15 +21,7 @@ Sollten Sie die Änderung nicht veranlasst haben, so ignorieren Sie einfach dies
 Viele Grüße
 
 Ihr Webmaster';
-
-// Change the title of the password reset e-mail
-function nxt_password_reset_title($title) {
-	return $message_title;
-}
-add_filter('retrieve_password_title', 'nxt_password_reset_title');
-
-// Change the body text of the password reset e-mail
-function nxt_retrieve_password_message( $message, $key, $user_login) {
+	
 	$password_reset_link = network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' );
 	return str_replace($message_template, "${password_reset_link}", $password_reset_link);
 }
